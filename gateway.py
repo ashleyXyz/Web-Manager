3c82c5aac8484f6250521713139c42637d693654
sliu208 <sophiel6@illinois.edu>
2026-01-31 20:45:48 -0600

    gemini update

--- gateway.py
+++ gateway.py
@@ -1,16 +1,17 @@
 import os
-from openai import OpenAI
+import google.generativeai as genai
 from keywordsai_tracing.decorators import workflow, task
 from keywordsai_tracing import KeywordsAITelemetry
+from dotenv import load_dotenv
+load_dotenv()  # reads .env into os.environ
+
 
 # Initialize Keywords AI Telemetry
-os.environ["KEYWORDSAI_API_KEY"] = "yf4XScSu.BddinxnSGyk0oy28sEvuV6PAN35ldkBj"
-os.environ["KEYWORDSAI_BASE_URL"] = "https://api.keywordsai.co/api"
-os.environ["OPENAI_API_KEY"] = "AIzaSyAiyC6YRIncqzOyh96a1HJ315-HjvhSNj0"
+# os.environ["KEYWORDSAI_API_KEY"]
+# os.environ["KEYWORDSAI_BASE_URL"]
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 k_tl = KeywordsAITelemetry()
-# Initialize OpenAI client
-client = OpenAI()
 
 @workflow(name="hello_world")
 def hello_world():
@@ -25,11 +26,9 @@ print(hello_world())
 class JokeAgent:
     @task(name="joke_creation")
     def create_joke(self):
-        completion = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[{"role": "user", "content": "Tell me a joke about tracing"}],
-        )
-        return completion.choices[0].message.content
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        response = model.generate_content("Tell me a joke")
+        return response.text
 
     def run(self):
         return self.create_joke()
